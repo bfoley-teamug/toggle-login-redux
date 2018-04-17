d3ce93c34502a60bb0f2b5f94ffda2c5847bdc1f
Brian Foley <bfoley-teamug@users.noreply.github.com>
2018-04-17 05:10:04 -0700

    initial commit

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Good Morning
+      <ul>
+        <li>Get up</li>
+        <li>Brush teeth</li>
+        <li>Shave</li>
+        <li>Shower</li>
+        <li>Kiss wife & kids goodbye</li>
+      </ul>
+    </div>
+  )
+}
